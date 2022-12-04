6c9491687a7684a5f9b1cbf86bd4780b90e37b0e
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-12-04 13:28:01 -0700

    cli menu, schema, seeds, have been created. User can view the different tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,18 @@
-You might also want to include a seeds.sql file to pre-populate your database, making the development of individual features much easier.
+INSERT INTO department (id, name)
+VALUES  (1, "Sale"),
+        (2, "Engineering"),
+        (3, "Finance"),
+        (4, "Legal");
+
+-- INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+-- VALUES  (1, "Tucker", "Wall", 1, 1);
+
+INSERT INTO roles (id, title, department_id, salary)
+VALUES  (1, "Sales Lead", 1, "100000"),
+        (2, "Salesperson", 1, "80000"),
+        (3, "Lead Engineer", 2, "150000"),
+        (4, "Software Engineer", 2, "120000"),
+        (5, "Account Manager", 3, "160000"),
+        (6, "Accountant", 3, "125000"),
+        (7, "Legal Team Lead", 4, "250000"),
+        (8, "Lawyer", 4, "190000");
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const query = require("./queries");
+const cTable = require("console.table");
 
 // Connect to database
 const db = mysql.createConnection(
@@ -17,7 +11,46 @@ const db = mysql.createConnection(
 		user: "root",
 		// MySQL password
 		password: "password",
-		database: "courses_db",
+		database: "company_db",
 	},
 	console.log(`Connected to the courses_db database.`)
 );
+function initializeAPP() {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				message: "Select an option from below",
+				name: "option",
+				choices: [
+					"View all departments",
+					"View all roles",
+					"View all employees",
+					"Add a department",
+					"Add an employee",
+					"Update an employee role",
+				],
+			},
+		])
+		.then((answers) => {
+			if (answers.option === "View all departments")
+				db.query("SELECT * FROM department", function (err, results) {
+					console.table(results);
+					initializeAPP();
+				});
+			if (answers.option === "View all roles")
+				db.query("SELECT * FROM roles", function (err, results) {
+					console.table(results);
+					initializeAPP();
+				});
+			if (answers.option === "View all employees")
+				db.query("SELECT * FROM employee", function (err, results) {
+					console.table(results);
+					initializeAPP();
+				});
+		});
+
+	
+}
+
+initializeAPP();
