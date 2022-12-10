826e5a84b37c309b96be951c032a860b37de1f9f
Aaron Morgan <aaron.morgan.7@outlook.com>
2022-12-10 12:43:54 -0700

    user can now add a role and view all employees

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-//const addDepartment = require("./query.js");
 const cTable = require("console.table");
 
 // Connect to database
@@ -28,6 +27,7 @@ const initApp = () => {
 					"View all roles",
 					"View all employees",
 					"Add a department",
+					"Add a role",
 					"Add an employee",
 					"Update an employee role",
 				],
@@ -46,21 +46,35 @@ const initApp = () => {
 					LEFT JOIN department on (department.id = role.department_id)
 					ORDER BY role.id`,
 					function (err, results) {
-						if (err)
-							console.log(err);
+						if (err) console.log(err);
 						console.table(results);
 						initApp();
 					}
 				);
 			}
 			if (answers.option === "View all employees") {
-				db.query("SELECT * FROM employee", function (err, results) {
-					console.table(results);
-					initApp();
-				});
+				db.query(
+					`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department,
+					role.salary, 
+					CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+					FROM employee
+					LEFT JOIN employee manager on manager.id = employee.manager_id
+					INNER JOIN role on (role.id = employee.role_id)
+					INNER JOIN department on (department.id = role.department_id)
+					ORDER BY employee.id`,
+					function (err, results) {
+						if (err) console.log(err);
+						console.table(results);
+						initApp();
+					}
+				);
 			}
 			if (answers.option === "Add a department") {
 				addDepartment();
+			}
+
+			if (answers.option === "Add a role") {
+				addRole();
 			}
 
 			if (answers.option === "Add an employee") {
@@ -93,6 +107,59 @@ const addDepartment = () => {
 			});
 	});
 };
+
+const roleNameSalary = () => {
+	return [
+		{
+			type: "input",
+			name: "roleName",
+			message: "What is the name of the role?",
+		},
+		{
+			type: "input",
+			name: "newSalary",
+			message: "What is the salary of the role?",
+		},
+	];
+};
+
+async function addRole() {
+	const newRole = await inquirer.prompt(roleNameSalary());
+	db.query(
+		`SELECT department.id, department.name FROM department;`,
+		async (err, res) => {
+			if (err) console.log(err);
+			const { department } = await inquirer.prompt([
+				{
+					name: "department",
+					type: "list",
+					choices: () => res.map((res) => res.name),
+					message: "Which department does the role belong to?",
+				},
+			]);
+			let departmentId;
+			for (const row of res) {
+				if (row.name === department) {
+					departmentId = row.id;
+					continue;
+				}
+			}
+			db.query(
+				`INSERT INTO role SET ?`,
+				{
+					title: newRole.roleName,
+					salary: newRole.newSalary,
+					department_id: departmentId,
+				},
+				(err, res) => {
+					if (err) console.log(err);
+					console.log(`\n${newRole.roleName} has been added.\n`);
+					initApp();
+				}
+			);
+		}
+	);
+}
 
 const addEmployee = () => {
 	return new Promise(() => {
